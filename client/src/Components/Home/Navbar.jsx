a29a62ad198b889414ddfa1710695bb2314c73d3
faustine-van <faustinemuhayemariya44@gmail.com>
2024-06-16 11:01:27 +0200

    working on navbar

--- client/src/Components/Home/Navbar.jsx
+++ client/src/Components/Home/Navbar.jsx
@@ -1,40 +1,115 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import './Navbar.css';
+import React, {useState} from "react";
+import { Link } from "react-router-dom";
+import {
+  FaLightbulb,
+  FaUser,
+  FaUsers,
+  FaBriefcase,
+  FaBook,
+  FaTools,
+  FaBuilding,
+  FaCaretDown,
+  FaSearch,
+  FaGraduationCap, FaDesktop, FaCalendarAlt, FaUserFriends,
+} from "react-icons/fa";
+
+import "./Navbar.css";
 
 function Navbar() {
-    return (
-        <div className='navbar'>
-            <div className="navbar-left">
-                <div className="logo-container">
-                    <img src="/techguide-logo.png" alt="TechGuide Hub Logo" height="50" />
-                </div>
-                <div className="navbar-menu">
-                    <a className="">Home</a>
-                    <a className="about">About Us</a>
-                    <a className="blog">Blog</a>
-                    <a className="events">Events</a>
-                    <a className="resources">Resources</a>
-                    <a className="mentorship">Mentorship Programs</a>
-                    <a className="contact">Contact Us</a>
-                </div>
-            </div>
-            <div className='navbar-right'>
-                <div className="search-container">
-                    <input type="text" placeholder="Search..." />
-                </div>
-                <div className="profile-container">
-                    <img src="/profile-icon.png" alt="User Profile" height="30" />
-                    <div className="profile-dropdown">
-                        <a className="profile">My Account</a>
-                        <a className="login">Login</a>
-                        <a className="signup">Sign Up</a>
-                        <a className="logout">Logout</a>
-                    </div>
-                </div>
-            </div>
+
+  const [showDropdown, setShowDropdown] = useState(false);
+  return (
+    <div className="navbar">
+      <div className="navbar-left">
+        <div className="logo-container">
+          <img src="/techguide-logo.png" alt="TechGuide Hub Logo" height="50" />
         </div>
-    );
+        <div className="navbar-menu">
+          <a className="link">Home</a>
+          <a className="link about">About Us</a>
+          <div className="dropdown">
+            <a className="link blog" onClick={setShowDropdown(true)}>Opportunities <FaCaretDown/></a>
+            { showDropdown &&
+             <div className="dropdown-content">
+             <div className="mega-menu">
+               <div className="mega-menu-column1">
+                 <a href="#" className="link">
+                   <FaGraduationCap className="icon" /> Scholarships
+                 </a>
+                 <a href="#" className="link">
+                   <FaBriefcase className="icon" /> Jobs
+                 </a>
+                 <a href="#" className="link">
+                   <FaDesktop className="icon" /> Online Courses
+                 </a>
+                 <a href="#" className="link">
+                   <FaCalendarAlt className="icon" /> Events
+                 </a>
+                 <a className="link">
+                   <FaUserFriends className="icon" /> Mentorship Programs
+                 </a>
+               </div>
+             </div>
+           </div>
+
+            }
+           
+          </div>
+          <div className="dropdown">
+            <a href="#" className="link">
+              Blog <FaCaretDown/>
+            </a>
+            <div className="dropdown-content">
+              <div className="mega-menu">
+                <div className="mega-menu-column">
+                  <a href="#1" className="link">
+                    {" "}
+                    <FaLightbulb className="icon" /> Career Tips{" "}
+                  </a>
+                  <a href="#2" className="link">
+                    {" "}
+                    <FaUsers className="icon" />
+                    Professional Insights{" "}
+                  </a>
+                  <a href="#3" className="link">
+                    <FaBriefcase className="icon" />
+                    Job Advice
+                  </a>
+                  <a href="#3" className="link">
+                    {" "}
+                    <FaBook className="icon" /> Learning Materials
+                  </a>
+                </div>
+                <div className="mega-menu-column">
+                  <a href="#job1" className="link">
+                    <FaBuilding className="icon" /> Developer Stories
+                  </a>
+                  <a href="#job2" className="link">
+                    <FaBook className="icon" /> Resources Articles
+                  </a>
+                  <a href="#job3" className="link">
+                    <FaTools className="icon" />
+                    Skill Building
+                  </a>
+                  <a href="#job3" className="link">
+                    <FaUser className="icon" />
+                    Developer Insights
+                  </a>
+                </div>
+              </div>
+            </div>
+          </div>
+          <a className="link contact">Contact Us</a>
+        </div>
+      </div>
+      <div className="navbar-right">
+        <div className="search-container">            
+          <input type="text" placeholder="Search articles..." />
+          <div className="search-icon"><FaSearch /></div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Navbar;
